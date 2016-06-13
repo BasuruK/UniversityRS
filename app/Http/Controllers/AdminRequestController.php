aeba3169b221360d397ea6809f85a0f8305bea47
Devin Amarasekara <devin.amarasekara@student.curtin.edu.au>
2016-06-13 23:03:12 +0530

    Commented and Organized the Code.

--- app/Http/Controllers/AdminRequestController.php
+++ app/Http/Controllers/AdminRequestController.php
@@ -9,6 +9,13 @@ use App\Http\Requests;
 
 class AdminRequestController extends Controller
 {
+
+    /**
+     * @return view(view to be redirected,collection of requests)
+     *
+     * This function creates the collection of requests and pass the collection to the
+     * Admin Request Management's main page.
+     */
     public function show()
     {
         $requests = \DB::table('requests')
@@ -30,6 +37,9 @@ class AdminRequestController extends Controller
         return view("adminRequests.admin_request_main",compact('requests'));
     }
 
+    /**
+     * @return to a new form
+     */
     public function newForm()
     {
         $users = \DB::table('users')->get();
@@ -39,6 +49,13 @@ class AdminRequestController extends Controller
         return view("adminRequests.admin_request_add", compact('batches','subjects','resources','users'));
     }
 
+    /**
+     * @param Request $request <- details of the new request from the form
+     * @return to the main page
+     *
+     * This function creates an object from the Admin_Request model, then assign the values
+     * received from the form and saves as a record in the database
+     */
     public function add(Request $request)
     {
 
@@ -64,12 +81,25 @@ class AdminRequestController extends Controller
         return Redirect::route('adminRequestShow');
     }
 
+    /**
+     * @param Admin_Request $admin_request <- record to be deleted
+     * @return to the main page
+     *
+     * This function deletes the specified record passed as a parameter
+     */
     public function delete(Admin_Request $admin_request)
     {
         Admin_Request::destroy($admin_request['id']);
         return redirect::route('adminRequestShow');
     }
 
+    /**
+     * @param Admin_Request $admin_request <- Request to be loaded to be edited
+     * @return to the edit form
+     *
+     * This function takes the request to be edited as the parameter and pass along with
+     * other collections as user,batch,subject and resource details to the edit form
+     */
     public function edit(Admin_Request $admin_request)
     {
         $users=\DB::table('users')->get();
@@ -80,7 +110,15 @@ class AdminRequestController extends Controller
         return view('adminRequests.admin_request_edit',compact('admin_request','batches','subjects','resources','users'));
 
     }
-    
+
+    /**
+     * @param Request $request <- modified details from the form
+     * @param Admin_Request $admin_request <- request to be updated
+     * @return to the main view
+     *
+     * This function updates the record in the database with the modified values
+     * taken from the edit form
+     */
     public function update(Request $request, Admin_Request $admin_request)
     {
         $admin_request->lecturerID=$request['selectstaffEdit'];
@@ -96,6 +134,14 @@ class AdminRequestController extends Controller
         return redirect::route('adminRequestShow');
     }
 
+    /**
+     * @param Admin_Request $admin_request <- details of the request
+     * @return to the main view
+     *
+     * This functions takes the details of the request as the parameter, then extracts details
+     * such as user email of the user who made the request and then creates the body of the request
+     * and send an email to the user's email address
+     */
     public function notify(Admin_Request $admin_request)
     {
 
--- app/Http/Controllers/BatchController.php
+++ app/Http/Controllers/BatchController.php
@@ -9,17 +9,35 @@ use Redirect;
 
 class BatchController extends Controller
 {
+    /**
+     * @return to the main page of the Batch Management
+     *
+     * This functions pass the main page and the collection of details regarding the batches
+     * through a view
+     */
     public function show()
     {
         $batches = \DB::table('batch')->get();
         return view("batches.batch_main",compact('batches'));
     }
-    
+
+    /**
+     * @return to a new form
+     *
+     * This functions returns the user a new form in which details can be filled out
+     */
     public function add()
     {
         return view("batches.batch_add");
     }
-    
+
+    /**
+     * @param Request $request <- details from the new form
+     * @return to the main page
+     *
+     * This functions takes the details from the new form as a parameter, and then creates an Batch object
+     * to which the values are assigned and saved as a record in the database
+     */
     public function addBatch(Request $request)
     {
         
@@ -39,13 +57,29 @@ class BatchController extends Controller
         
         return Redirect::route('batchShow');
     }
-    
+
+    /**
+     * @param Batch $batch <- details of the batch to be edited
+     * @return to the edit form
+     *
+     * This function takes the details of the batch to be edited as a parameter and pass
+     * them to the edit form, and the details are displayed in the edit form accordingly
+     */
     public function edit(Batch $batch)
     {
         //return $batch;
         return view('batches.batch_edit',compact('batch'));
     }
-    
+
+    /**
+     * @param Request $request <- modified details from the edit form
+     * @param Batch $batch <- original details of the batch
+     * @return to the main page
+     *
+     * This functions takes the modified details and the original details of the batch as
+     * parameters and then assign the new values to the batch object. Then it updates the relevant
+     * record in the database
+     */
     public function update(Request $request, Batch $batch)
     {
         //$batch->update($request->all());
@@ -57,7 +91,14 @@ class BatchController extends Controller
         
         return Redirect::route('batchShow');
     }
-    
+
+    /**
+     * @param Batch $batch <- details of the batch to be deleted
+     * @return to the main page
+     *
+     * This function takes the batch to be deleted and carries out the destroy function which deletes
+     * the relevant record from the database
+     */
     public function delete(Batch $batch)
     {
         Batch::destroy($batch['id']);
