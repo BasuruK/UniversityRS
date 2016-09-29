9e7a0a18585e5df75e0fc907e36e991e905ff8db
Devin Amarasekara <devin.amarasekara@student.curtin.edu.au>
2016-09-29 19:32:28 +0530

    Duplicate entries are handled.

--- resources/views/batches/batch_add.blade.php
+++ resources/views/batches/batch_add.blade.php
@@ -18,6 +18,15 @@
                 </div>
             @endif
 
+        <div class="flash-message">
+            @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+                @if(Session::has('alert-' . $msg))
+
+                    <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
+                @endif
+            @endforeach
+        </div> <!-- end .flash-message -->
+
             <!-- form start -->
             <form role="form" method="POST" action="{{ url('/batch/batch_add') }}">
                 
