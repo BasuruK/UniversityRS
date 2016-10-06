ad7695f52c49208ccc66219e6f7bfc05766317eb
VishBandara <shaydenb31@gmail.com>
2016-10-06 19:17:13 +0530

    Generate timetable for resources function implemented

---
+++ app/Http/Controllers/resourceTimeTableController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\TimeFormatTable;
+use App\Http\Requests;
+use App\TimeTable;
+use DB;
+use Excel;
+
+class resourceTimeTableController extends Controller
+{
+    public function index(Request $request)
+    {
+
+        $hallNo=$request->hallNo;
+        $ResourceTime  = TimeTable::where('resourceName','=',$hallNo)->get()->toArray();
+        $fullTable      = TimeFormatTable::all();
+
+      
+
+        return view('timeTable.resourceTimetable')->with('fullTimeTable',$fullTable)->with('ResourceTimeDetails',$ResourceTime)->with('hallNo',$hallNo);
+    }
+}
