b42b1cb8478f497e3c38508eaabd9c54b3906bf5
Basuru Kusal Balasuriya <basuru.balasuriya@student.curtin.edu.au>
2016-10-29 20:19:22 +0530

    fixed administrator timetable management designe issues

--- resources/views/timeTable/timeTable.blade.php
+++ resources/views/timeTable/timeTable.blade.php
@@ -13,90 +13,101 @@
 @endsection
 
 @section('content')
-    <form role="form" method="POST" action="/timetable/batchTimetableForm/batch_Timetable" name="batchTimetableForm" id="batchTimetableForm">
-        <div class="form-group">
-            <label>Year</label>
-            <select class="form-control" name="selectyear" id="selectyear">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-            </select>
-        </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-info">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Add Users <small> *Users who are allowed to register</small></h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <form role="form" method="POST" action="/timetable/batchTimetableForm/batch_Timetable" name="batchTimetableForm" id="batchTimetableForm">
+                        <div class="form-group">
+                            <label>Year</label>
+                            <select class="form-control" name="selectyear" id="selectyear">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                            </select>
+                        </div>
 
 
-        <script>
-            /**
-             * Dynamically populate the select options for batches
-             */
-            $(document).ready(function()
-            {
-                $.get("{{ url('/timetable/batchTimetableForm/loadBatches')}}", {option: $('#selectyear').val()},
+                        <script>
+                            /**
+                             * Dynamically populate the select options for batches
+                             */
+                            $(document).ready(function()
+                            {
+                                $.get("{{ url('/timetable/batchTimetableForm/loadBatches')}}", {option: $('#selectyear').val()},
 
-                        function(data) {
+                                        function(data) {
 
-                            var selectedbatch = $('#selectbatch');
+                                            var selectedbatch = $('#selectbatch');
 
-                            selectedbatch.empty();
+                                            selectedbatch.empty();
 
-                            $.each(data, function(key, value) {
+                                            $.each(data, function(key, value) {
 
-                                selectedbatch
+                                                selectedbatch
 
-                                        .append($("<option></option>")
+                                                        .append($("<option></option>")
 
-                                                .attr("value",key)
+                                                                .attr("value",key)
 
-                                                .text(value));
-                            });
+                                                                .text(value));
+                                            });
 
-                        });
-                $('#selectyear').change(function(){
+                                        });
+                                $('#selectyear').change(function(){
 
-                    $.get("{{ url('/timetable/batchTimetableForm/loadBatches')}}", {option: $(this).val()},
+                                    $.get("{{ url('/timetable/batchTimetableForm/loadBatches')}}", {option: $(this).val()},
 
-                            function(data) {
+                                            function(data) {
 
-                                var selectedbatch = $('#selectbatch');
+                                                var selectedbatch = $('#selectbatch');
 
-                                selectedbatch.empty();
+                                                selectedbatch.empty();
 
-                                $.each(data, function(key, value) {
+                                                $.each(data, function(key, value) {
 
-                                    selectedbatch
+                                                    selectedbatch
 
-                                            .append($("<option></option>")
+                                                            .append($("<option></option>")
 
-                                                    .attr("value",key)
+                                                                    .attr("value",key)
 
-                                                    .text(value));
+                                                                    .text(value));
+                                                });
+
+                                            });
+
                                 });
 
                             });
 
-                });
 
-            });
+                        </script>
+
+                        <!-- select Batch -->
+                        <div class="form-group">
+                            <label>Batch</label>
+                            <select class="form-control" name="selectbatch" id="selectbatch">
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="box-footer">
+                                <button type="submit" class="btn btn-primary pull-right">View Timetable</button>
+                            </div>
+                        </div>
 
 
-        </script>
-
-        <!-- select Batch -->
-        <div class="form-group">
-            <label>Batch</label>
-            <select class="form-control" name="selectbatch" id="selectbatch">
-            </select>
-        </div>
-
-        <div class="form-group">
-            <div class="box-footer">
-                <button type="submit" class="btn btn-primary">View Timetable</button>
+                        {!! csrf_field() !!}
+                    </form>
+                </div><!-- /.box-body -->
             </div>
         </div>
-        </div>
-        <!-- /.box-body -->
-
-        {!! csrf_field() !!}
-    </form>
-
+    </div> <!-- ./row -->
 @endsection
