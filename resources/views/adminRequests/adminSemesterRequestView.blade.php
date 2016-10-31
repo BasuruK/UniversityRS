647ae61766da89ed57d398877ef7f9068f97ea89
Devin Amarasekara <devin.amarasekara@student.curtin.edu.au>
2016-10-31 12:25:24 +0530

    Semester Request Management UI Fixes.

--- resources/views/adminRequests/adminSemesterRequestView.blade.php
+++ resources/views/adminRequests/adminSemesterRequestView.blade.php
@@ -6,8 +6,8 @@
             Semester Request Management
         </h1>
         <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-            <li class="active">Overview</li>
+            <li><a href="#"><i class="fa fa-dashboard"></i>Home</a></li>
+            <li class="active">Semester Requests</li>
         </ol>
     </section>
 @endsection
