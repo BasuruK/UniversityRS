11de6673a4a8a288750aad129bcfd4871b3b810a
Devin Amarasekara <devin.amarasekara@student.curtin.edu.au>
2016-10-31 12:25:49 +0530

    Formal Request Management main window updated.

--- resources/views/adminRequests/admin_request_main.blade.php
+++ resources/views/adminRequests/admin_request_main.blade.php
@@ -1,13 +1,13 @@
-@extends('layouts.main')
+@extends('layouts.Main')
 
 @section('section-header')
     <section class="content-header">
         <h1>
-            Request Management
+            Formal Request Management
         </h1>
         <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-            <li class="active">Overview</li>
+            <li><a href="#"><i class="fa fa-dashboard"></i>Home</a></li>
+            <li class="active">Formal Requests</li>
         </ol>
     </section>
 @endsection
@@ -17,12 +17,7 @@
         <div class="row">
             <div class="box">
                 <div class="box-header">
-                    <h3 class="box-title">Requests</h3>
-                    <tr>
-                        <td><a href='/adminRequest/newForm' id="btnAdd" class="btn btn-primary pull-right">Add Request</a></td>
-                        <td><a href='/adminRequest/BatchSort' id="btnDateSort" class="btn btn-primary">Sort By Batch</a></td>
-                    </tr>
-
+                    <h3 class="box-title">Formal Requests</h3>
                 </div>
                 <!-- /.box-header -->
                 <div class="box-body">
@@ -35,16 +30,15 @@
                             <div class="col-sm-12">
                                 <script>
                                     /**
-                                     * Initialise DataTable for Registered Users
+                                     * Initialise DataTable for Pending requests
                                      */
                                     $(document).ready(function() {
                                         $('#dataTableRegUsers').DataTable();
-
                                     } );
 
-
-
-
+                                    /**
+                                     * Confirmation messages for delete and edit
+                                     */
                                     function isDelete(id)
                                     {
                                         var ID =id;
@@ -52,11 +46,11 @@
                                             theme: 'black',
                                             title: 'Confirm Deletion',
                                             icon: 'fa fa-warning',
-                                            content: 'Are you sure want to remove this request?',
+                                            content: 'Are you sure want to remove this Formal Request?',
                                             confirmButton: 'Yes',
                                             confirmButtonClass: 'btn-danger',
                                             confirm: function(){
-                                                location.href="/adminRequest/delete/"+ID;
+                                                location.href="/adminRequest/delete./"+ID;
                                             }
 
                                         });
@@ -69,7 +63,7 @@
                                             theme: 'black',
                                             title: 'Confirm Deletion',
                                             icon: 'fa fa-warning',
-                                            content: 'Are you sure want to Edit this request?',
+                                            content: 'Are you sure want to Edit this Formal Request?',
                                             confirmButton: 'Yes',
                                             confirmButtonClass: 'btn-danger',
                                             confirm: function(){
@@ -79,19 +73,24 @@
                                         });
                                         return false;
                                     }
+                                    {{--function LoadEdit(id) {--}}
+                                    {{--var ID=id;--}}
+
+                                    {{--$.get("{{ url('/userRequest/Edit/loadEditDetails')}}", {option:id})--}}
+                                    {{--}--}}
+
                                 </script>
                                 <table id="dataTableRegUsers" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="dataTableRegUsers_info">
                                     <thead>
                                     <tr role="row">
                                         <th class="sorting_asc" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Staff ID: activate to sort column descending" style="width: 55px;" aria-sort="ascending">Request ID</th>
-                                        <th class="sorting_asc" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Staff ID: activate to sort column descending" style="width: 55px;" aria-sort="ascending">Staff Member</th>
-                                        <th class="sorting_asc" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Staff ID: activate to sort column descending" style="width: 55px;" aria-sort="ascending">Hall Number</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Prefix: activate to sort column ascending" style="width: 45px;">Lecturer</th>
                                         <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Prefix: activate to sort column ascending" style="width: 45px;">Batch</th>
                                         <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Name: activate to sort column ascending" style="width: 110px;">Year</th>
                                         <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Name: activate to sort column ascending" style="width: 110px;">Subject</th>
-                                        <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Name: activate to sort column ascending" style="width: 110px;">Time Slot</th>
                                         <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Name: activate to sort column ascending" style="width: 110px;">Date</th>
-                                        <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Name: activate to sort column ascending" style="width: 110px;">Status</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Name: activate to sort column ascending" style="width: 110px;">Time Slot</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Name: activate to sort column ascending" style="width: 110px;">Resource Type</th>
                                         <th tabindex="0"  rowspan="1" colspan="1" aria-label="Edit/ Delete" style="width: 60px;">Edit/ Delete</th>
                                     </tr>
                                     </thead>
@@ -99,19 +98,18 @@
                                     @foreach($requests as $request)
                                         <tr role="row" class="odd">
                                             <td class="sorting_1">{{$request->id}}</td>
-                                            <td>{{$request->name }}</td>
-                                            <td>{{$request->hallNo}}</td>
+                                            <td>{{$request->name}}</td>
                                             <td>{{$request->batchNo}}</td>
                                             <td>{{$request->year}}</td>
                                             <td>{{$request->subName}}</td>
-                                            <td>{{$request->timeSlot}}</td>
                                             <td>{{$request->requestDate}}</td>
-                                            <td>{{$request->status}}</td>
+                                            <td>{{$request->timeSlot}}</td>
+                                            <td>{{$request->ResourceType}}</td>
                                             <td>
-                                                <a  onclick="return isEdit({{$request->id}})" class="btn btn-info">Edit</a>
-                                                <a class="btn btn-danger" onclick="return isDelete({{$request->id}})">Delete</a>
+                                                <a class="btn btn-primary " onclick="return isEdit({{$request->id}})">Edit</a>
+                                                <br>
+                                                <a class="btn btn-danger " onclick="return isDelete({{$request->id}})">Delete</a>
                                             </td>
-                                            <td><a href="/adminRequest/notify/{{ $request->id }}" class="btn btn-warning">Notify</a></td>
                                         </tr>
                                     @endforeach
                                     </tbody>
@@ -130,15 +128,78 @@
                 </div>
                 <!-- /.box-body -->
             </div> <!--/.box-->
+        </div><!--/.row-->
+        <!----second box-->
+        <div class="row">
+
+            <div class="box">
+                <div class="box-header">
+                    <h3 class="box-title">Accepted Formal Requests</h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <div id="dataTableRegUsers_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+                        <div class="row">
+                            <div class="col-sm-6"></div>
+                            <div class="col-sm-6"></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <script>
+                                    /**
+                                     * Initialise DataTable for Accepted requests Users
+                                     */
+                                    $(document).ready(function() {
+                                        $('#dataTableAcceptedFormalRequests').DataTable();
+                                    } );
+                                </script>
+                                <table id="dataTableAcceptedFormalRequests" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="dataTableRegUsers_info">
+                                    <thead>
+                                    <tr role="row">
+                                        <th class="sorting_asc" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Staff ID: activate to sort column descending" style="width: 55px;" aria-sort="ascending">Request ID</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Name: activate to sort column ascending" style="width: 110px;">Name</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Name: activate to sort column ascending" style="width: 110px;">Batch</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Email: activate to sort column ascending" style="width: 140px;">Year</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Email: activate to sort column ascending" style="width: 140px;">Subject</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Name: activate to sort column ascending" style="width: 110px;">Date</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Email: activate to sort column ascending" style="width: 140px;">Time Slot</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTableRegUsers" rowspan="1" colspan="1" aria-label="Email: activate to sort column ascending" style="width: 140px;">Resource ID</th>
+
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($acceptedrequests as $acceptedrequest)
+                                        <tr role="row" class="odd">
+                                            <td class="sorting_1">{{ $acceptedrequest->id }}</td>
+                                            <td>{{ $acceptedrequest->name }}</td>
+                                            <td>{{ $acceptedrequest->batchNo }}</td>
+                                            <td>{{ $acceptedrequest->year }}</td>
+                                            <td>{{ $acceptedrequest->subName }}</td>
+                                            <td>{{$acceptedrequest->requestDate}}</td>
+                                            <td>{{ $acceptedrequest->timeSlot }}</td>
+                                            <td>{{ $acceptedrequest->resourceID }}</td>
+
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <div class="dataTables_info" id="dataTableRegUsers_info" role="status" aria-live="polite"></div>
+                            </div>
+                            <div class="col-sm-7">
+                                <div class="dataTables_paginate paging_simple_numbers" id="dataTableRegUsers_paginate"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.box-body -->
+            </div> <!--/.box-->
+
+
+
         </div>
-
-        <!-- Cancelled Request Table -->
-
-
     </div>
-
-
-
-
-
 @endsection
